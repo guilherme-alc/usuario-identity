50061cf691a668bce287dec2e7bab745bfcb5f11
guilherme-alc <guilhermealc01@gmail.com>
2024-07-29 20:44:58 -0300

    Gerando token com jwt

---
+++ Services/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuarioIdentity.Models;
+
+namespace UsuarioIdentity.Services;
+
+public class TokenService
+{
+    internal void GenerateToken(Usuario usuario)
+    {
+
+        Claim[] claims = new Claim[]
+        {
+            new Claim("username", usuario.UserName),
+            new Claim("id", usuario.Id),
+            new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString()),
+        };
+
+        var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ASD91U9DJDASODI02FHDT"));
+
+        var signingCredentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken
+            (
+                expires: DateTime.Now.AddMinutes(10),
+                claims: claims,
+                signingCredentials: signingCredentials
+            );
+    }
+}
